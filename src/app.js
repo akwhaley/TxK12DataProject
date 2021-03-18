e08c6eeaa3b6d90011a18657335d244141d401e4
Amanda <>
2021-03-18 18:37:47 -0500

    add legend to dumbell plot

--- src/app.js
+++ src/app.js
@@ -18,22 +18,17 @@ const plotHeight = height - margin.top - margin.bottom;
 const colors = ['#940000', '#c9a305', '#3a3838', '#a9a9a9', '#87762f',
                 '#367c54', '#334e8b', '#ff6a6a']
 
+//Bring in data
 Promise.all([
   fetch('./data/campus.json').then(x => x.json()),
   fetch('./data/district.json').then(x => x.json())
 ]).then(myVis);
 
-//Some helpful functions
+//Function definition
 function unique(data, key) {
   return Array.from(data.reduce((acc, row) => acc.add(row[key]), new Set()));
 }
 
-function sortByKey(data, key) {
-  data.sort((a, b) => {
-    return a.key - b.key;
-});
-
-}
 
 function myVis(data) {
   const [campusData, districtData] = data;
@@ -43,6 +38,7 @@ function myVis(data) {
                    'LEPPct', 'OverperformanceScoreMean']
   console.log(districtData);
 
+  //Reformat campus data as an Object of objects where keys are district ids.
   let campuses = campusData.reduce((acc, row) => {
     if (!acc.hasOwnProperty(row['DistrictID'])) {
         acc[row['DistrictID']] = [];
@@ -50,11 +46,12 @@ function myVis(data) {
     acc[row['DistrictID']].push(row);
     return acc;
   }, {});
-  console.log(campuses);
 
+  //Default columns for x and y axis.
   let xCol = columns[6];
   let yCol = columns[2];
 
+  //Content left side panel
   const context = select('#app')
     .append('div')
     .attr('id', 'context')
@@ -72,6 +69,7 @@ function myVis(data) {
     .append('html')
     .html("<p>Every public school in Texas receives an overall rating score between 0 and 100 every school year.  The overall score is a combination of three domain scores - Student Achievement, Academic Growth, and Closing the Gaps.  More information can be found <a href=https://tea.texas.gov/texas-schools/accountability/academic-accountability>here</a>.</p><p>The scatterplot allows you to explore how the (weighted) average scores for every school district in the state relate to the characterstics of the students in that district.  A good rating system should capture the quality of education regardless of the demographics of the students attending each school.  We can see from this data that for schools with lower fractions of students who are deemed 'economically disadvantaged', we see that most have higher scores in all categories.  For schools with higher fractions, we see a wider range of scores.</p><p>This model uses a school's student characteristics and predicts its Overall Score based on the average score for similar schools.  The 'overperformance' score is the difference between the actual score and the predicted score.  Clicking on a data point will open a panel below with more information.</p>")
 
+  //Scatterplot
   const scatterPlot = select('#app')
     .append('div')
     .style('display', 'flex')
@@ -112,14 +110,12 @@ function myVis(data) {
     .attr('class', 'chart-container')
     .style('position', 'relative');
 
-  //Creates chart container
   const svg = svgContainer
     .append('svg')
     .attr('height', height)
     .attr('width', width)
     .append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
 
   const xAxis = svg
     .append('g')
@@ -161,7 +157,6 @@ function myVis(data) {
   leg.append('text')
     .text('Legend');
 
-
   var i;
   for (i = 0; i < districtTypes.length; i++) {
     leg.append("rect")
@@ -184,23 +179,25 @@ function myVis(data) {
     .attr('id', 'tooltip')
     .style('display', 'none');
 
-  function clickAndLog(data) {
-
+  function clickAndPlot(data) {
+    //If the bottom panel already exists, remove it.
     select('#bottom-panel').remove();
+
+    //Isolate data from selected district and sort by Overperformance.
     const schoolData = campuses[data.DistrictID].sort((a, b) => {
       return a.Overperformance - b.Overperformance;
     });
     console.log(schoolData);
 
+    //Define variables
     const plotWidth = 500;
-    const margin = {top: 50, left: 200, right: 10, bottom: 40};
-
+    const margin = {top: 70, left: 200, right: 10, bottom: 40};
     const schools = unique(schoolData, 'CampusName');
     const plotHeight = 20*schools.length;
-
     const width = plotWidth + margin.left + margin.right;
     const height = plotHeight + margin.top + margin.bottom;
 
+    //Scales
     const yScale = scaleBand()
       .domain(schools)
       .range([0, plotHeight]);
@@ -209,6 +206,9 @@ function myVis(data) {
       .domain([50, 100])
       .range([0, plotWidth]);
 
+    const colorScale = scaleOrdinal()
+      .domain(['+', '-'])
+      .range(['#aaa','#940000'])
 
     const bottomPanel = select('#app')
       .append('div')
@@ -227,10 +227,64 @@ function myVis(data) {
       .attr('height', height)
       .attr('transform', `translate(0,0)`);
 
+//Legend
+    const l = svg
+      .append("g")
+      .attr('class', 'dumbell-legend')
+      .attr('transform', `translate(${margin.left}, 5)`)
+      .attr('height', 10)
+      .attr('width', plotWidth)
+
+    l.append('circle')
+      .attr('transform', `translate(0, 0)`)
+      .attr("fill", "#3a3838")
+      .attr("r", 3.5);
+
+    l.append('text')
+      .attr('transform', `translate(5, 5)`)
+      .style('font-size', 11)
+      .text('Actual Score')
+
+    l.append('circle')
+      .attr('transform', `translate(100, 0)`)
+      .attr("fill", "#334e8b")
+      .attr("r", 3.5);
+
+    l.append('text')
+      .attr('transform', `translate(105, 5)`)
+      .style('font-size', 11)
+      .text('Predicted Score')
+
+    l.append('line')
+      .attr("stroke", d => colorScale('-'))
+      .style("stroke-width", 2)
+      .attr("x1", 210)
+      .attr("y1", 2)
+      .attr("x2", 240)
+      .attr("y2", 2);
+
+    l.append('text')
+      .attr('transform', `translate(245, 5)`)
+      .style('font-size', 11)
+      .text('Actual < Predicted')
+
+    l.append('line')
+      .attr("stroke", d => colorScale('+'))
+      .style("stroke-width", 2)
+      .attr("x1", 370)
+      .attr("y1", 2)
+      .attr("x2", 400)
+      .attr("y2", 2);
+
+    l.append('text')
+      .attr('transform', `translate(405, 5)`)
+      .style('font-size', 11)
+      .text('Actual > Predicted')
+
     svg
       .append('g')
       .attr('class', 'x-axis')
-      .attr('transform', `translate(${margin.left}, ${margin.top/2})`)
+      .attr('transform', `translate(${margin.left}, ${2*margin.top/3})`)
       .call(axisTop(xScale));
 
     const g = svg.append("g")
@@ -244,17 +298,22 @@ function myVis(data) {
         .attr("transform", (d, i) => `translate(0,${yScale(d.CampusName)})`);
 
     g.append("line")
-        .attr("stroke", "#aaa")
+        .attr("stroke", d => colorScale(d.OverUnder))
+        .style("stroke-width", 2)
         .attr("x1", d => xScale(d.OverallScore))
-        .attr("x2", d => xScale(d.ModelOverallScore));
+        .attr("y1", -5)
+        .attr("x2", d => xScale(d.ModelOverallScore))
+        .attr("y2", -5);
 
     g.append("circle")
         .attr("cx", d => xScale(d.OverallScore))
-        .attr("fill", "#940000")
+        .attr("cy", -5)
+        .attr("fill", "#3a3838")
         .attr("r", 3.5);
 
     g.append("circle")
         .attr("cx", d => xScale(d.ModelOverallScore))
+        .attr("cy", -5)
         .attr("fill", "#334e8b")
         .attr("r", 3.5);
 
@@ -268,8 +327,6 @@ function myVis(data) {
       .attr('transform', `translate(${margin.left}, ${height-margin.bottom})`)
       .call(axisBottom(xScale));
 
-    //xAxis.call(axisBottom(xScale));
-
   }
 
   function renderChart() {
@@ -280,7 +337,6 @@ function myVis(data) {
     const yScale = scaleLinear()
       .domain(extent(districtData, d => d[yCol]))
       .range([plotHeight, 0]);
-
 
     xAxis.call(axisBottom(xScale));
     yAxis.call(axisLeft(yScale));
@@ -321,160 +377,11 @@ function myVis(data) {
                 'Special Education: ' + d.SPEDPct + '%' + "<br/>"),
       )
       .on('mouseleave', (e, d) => tooltip.style('display', 'none'))
-      .on('click', (e, d) => clickAndLog(d))
+      .on('click', (e, d) => clickAndPlot(d))
       .attr('fill', (_, idx) => interpolateTurbo(idx / 406))
       .attr('r', 3);
 
   }
   renderChart();
 
-  /*
-  const columns = Object.keys(districtData[0]);
-  let xCol = columns[1];
-  let yCol = columns[2];
-
-  const dropdowns = select('#app')
-    .append('div')
-    .style('display', 'flex')
-    .selectAll('.drop-down')
-    .data(['xCol', 'yCol'])
-    .join('div');
-
-  dropdowns.append('div').text(d => d);
-
-  dropdowns
-    .append('select')
-    .on('change', (event, row) => {
-      if (row === 'xCol') {
-        xCol = event.target.value;
-      } else {
-        yCol = event.target.value;
-      }
-      renderChart();
-    })
-    .selectAll('option')
-    .data(dim => columns.map(column => ({column, dim})))
-    .join('option')
-    .text(d => d.column)
-    .property('selected', d =>
-      d.dim === 'xCol' ? d.column === xCol : d.column === yCol,
-    );
-
-  const svgContainer = select('#app')
-    .append('div')
-    .attr('class', 'chart-container')
-    .style('position', 'relative');
-
-  const svg = svgContainer
-    .append('svg')
-    .attr('height', height)
-    .attr('width', width)
-    .append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-  const xAxis = svg
-    .append('g')
-    .attr('class', 'x-axis')
-    .attr('transform', `translate(0, ${plotHeight})`);
-
-  const yAxis = svg.append('g').attr('class', 'y-axis');
-
-  const xLabel = svg
-    .append('g')
-    .attr('class', 'x-axis-label')
-    .attr('transform', `translate(${plotWidth / 2}, ${height - 20})`)
-    .append('text')
-    .attr('text-anchor', 'middle');
-
-  const yLabel = svg
-    .append('g')
-    .attr('class', 'y-axis-label')
-    .attr('transform', `translate(-35, ${plotHeight / 2})`)
-    .append('text')
-    .attr('text-anchor', 'middle')
-    .attr('transform', `rotate(-90)`);
-
-  const tooltip = svgContainer
-    .append('div')
-    .attr('id', 'tooltip')
-    .style('display', 'none');
-
-  function renderChart() {
-    const t = transition().duration(300);
-    const xScale = scaleLinear()
-      .domain(extent(data, d => d[xCol]))
-      .range([0, plotWidth]);
-    const yScale = scaleLinear()
-      .domain(extent(data, d => d[yCol]))
-      .range([plotHeight, 0]);
-
-    xAxis.call(axisBottom(xScale));
-    yAxis.call(axisLeft(yScale));
-    xLabel.text(xCol);
-    yLabel.text(yCol);
-
-    svg
-      .selectAll('circle')
-      .data(data)
-      .join(
-        enter =>
-          enter
-            .append('circle')
-            .attr('cx', d => xScale(d[xCol]))
-            .attr('cy', d => yScale(d[yCol])),
-        update =>
-          update.call(el =>
-            el
-              .transition(t)
-              .attr('cx', d => xScale(d[xCol]))
-              .attr('cy', d => yScale(d[yCol])),
-          ),
-      )
-      .on('mouseenter', (e, d) =>
-        tooltip
-          .style('display', 'block')
-          .style('left', `${e.offsetX}px`)
-          .style('top', `${e.offsetY}px`)
-          .text(d.Name),
-      )
-      .on('mouseleave', (e, d) => tooltip.style('display', 'none'))
-      .attr('fill', (_, idx) => interpolateTurbo(idx / 406))
-      .attr('r', 5);
-  }
-  renderChart();
-*/
-}
-
-
-
-function myVisMap(results) {
-  const [campusData, districtData, districtBoundaries] = results;
-  console.log(campusData);
-  console.log(districtData);
-  console.log(districtBoundaries);
-  const columns = Object.keys(districtData[0]);
-  console.log(columns);
-
-
-  var projection = geoEquirectangular()
-    .fitExtent([[0,0], [800,800]], districtBoundaries);
-  var geoGenerator = geoPath()
-    .projection(projection);
-
-  // Join the FeatureCollection's features array to path elements
-  var u = select('#app g.map')
-    .selectAll('path')
-    .data(districtBoundaries.features);
-
-  // Create path elements and update the d attribute using the geo generator
-  u.enter()
-    .append('path')
-    .attr('d', geoGenerator);
-
-  // EXAMPLE FIRST FUNCTION
-  /*
-  select('#app')
-    .append('h1')
-    .text('hi!');
-  */
 }
